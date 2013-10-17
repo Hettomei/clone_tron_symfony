659e1e46b7f0d80b12854760d304b96c668195d7
Hettomei <itsumo.sibyllin@gmail.com>
2013-10-17 12:29:41 +0200

    add the ability to log people

---
+++ src/Tim/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tim\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class SecurityController extends Controller
+{
+  // Taken here :
+  // http://fr.openclassrooms.com/informatique/cours/developpez-votre-site-web-avec-le-framework-symfony2/premiere-approche-de-la-securite
+  public function loginAction()
+  {
+    // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+    if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+      return $this->redirect($this->generateUrl('tim_tron_homepage'));
+    }
+
+    $request = $this->getRequest();
+    $session = $request->getSession();
+
+    // On vérifie s'il y a des erreurs d'une précédente soumission du formulaire
+    if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+      $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+    } else {
+      $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+      $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+    }
+
+    return $this->render('TimUserBundle:Security:login.html.twig', array(
+      // Valeur du précédent nom d'utilisateur entré par l'internaute
+      'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+      'error'         => $error,
+    ));
+  }
+}
