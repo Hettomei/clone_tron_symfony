128830bc22cbadf8ce4de03a9a1d352e2099ac5c
Hettomei <itsumo.sibyllin@gmail.com>
2013-10-17 17:06:18 +0200

    Add a signup page. But without the logic

--- src/Tim/UserBundle/Controller/SecurityController.php
+++ src/Tim/UserBundle/Controller/SecurityController.php
@@ -33,4 +33,38 @@ class SecurityController extends Controller
       'error'         => $error,
     ));
   }
+
+  public function signupAction()
+  {
+    // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+    if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+      return $this->redirect($this->generateUrl('tim_tron_homepage'));
+    }
+
+    $request = $this->getRequest();
+    $session = $request->getSession();
+
+    // On vérifie s'il y a des erreurs d'une précédente soumission du formulaire
+    if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+      $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+    } else {
+      $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+      $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+    }
+
+    return $this->render('TimUserBundle:Security:signup.html.twig', array(
+      // Valeur du précédent nom d'utilisateur entré par l'internaute
+      'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+      'error'         => $error,
+    ));
+  }
+
+  public function signupCheckAction()
+  {
+    return $this->render('TimUserBundle:Security:signup.html.twig', array(
+      // Valeur du précédent nom d'utilisateur entré par l'internaute
+      'last_username' => "LOL",
+      'error'         => null,
+    ));
+  }
 }
